688e2b21c5c3a6958f77c14a265045791972861f
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-07-09 10:38:08 +0200

    fixed commantary table added fake commentary Joined user and commentary table

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,10 +1,13 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -43,6 +46,9 @@ public class User {
     @Column(columnDefinition = "TEXT")
     private String biography;
     private boolean isOnline;
+
+    @OneToMany(mappedBy = "user")
+    private List<Commentary> commentaries;
 
     public User() {}
 
